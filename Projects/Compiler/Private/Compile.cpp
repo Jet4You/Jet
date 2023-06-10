58a43c106536e3f38ac3c27a099ffcde87917a57
PoetaKodu <poetakodupl@gmail.com>
2023-06-10 17:10:34 +0200

    improve: basic mocked compilation step fix: wrong array size in mocked IR code

--- Projects/Compiler/Private/Compile.cpp
+++ Projects/Compiler/Private/Compile.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <filesystem>
+#include <cstdlib>
 
 import RigC.Compiler.Compile;
 import RigC.Compiler.Settings;
 import RigC.Core.StdTypes;
 import RigC.Core.Result;
+import RigC.Core.File;
 
 namespace rigc::compiler
 {
@@ -13,9 +16,11 @@ using parser::ParseResult;
 using LLVMGenError = String;
 
 static auto generate_llvm_ir(ParseResult const& parse_result, Settings const& settings) -> Result<String, LLVMGenError>;
-static auto ensure_intermediate_directory_exists(Settings const& settings) -> void;
+static auto ensure_exists(Path const& directory_path) -> void;
 static auto cleanup_intermediate_directory(Settings const& settings) -> void;
 static auto determine_intermediate_directory(Settings const& settings) -> Path;
+static auto generate_intermediate_content(Path const& dir_path, StringView content) -> void;
+static auto run_llvm_compilation(Path const& directory) -> void;
 
 auto compile(ParseResult parse_result, Settings settings) -> Result<int, CompileError>
 {
@@ -26,7 +31,10 @@ auto compile(ParseResult parse_result, Settings settings) -> Result<int, Compile
     return error("LLVM IR generation failed");
   }
 
-  ensure_intermediate_directory_exists(settings);
+  auto intermediate_directory = determine_intermediate_directory(settings);
+  ensure_exists(intermediate_directory);
+
+  generate_intermediate_content(intermediate_directory, maybe_ir.get_unchecked());
 
   if (settings.should_cleanup_intermediate()) {
     cleanup_intermediate_directory(settings);
@@ -44,9 +52,11 @@ static auto generate_llvm_ir(ParseResult const& parse_result, Settings const& se
     "declare i32 @puts(i8* nocapture) nounwind\n"
     "\n"
     "define i32 @main() {\n"
-    "    %1 = call i32 @puts(i8* getelementptr inbounds ([20 x i8], [20 x i8]* @.str, i32 0, i32 0))\n"
+    "    %1 = call i32 @puts(i8* getelementptr inbounds ([23 x i8], [23 x i8]* @.str, i32 0, i32 0))\n"
     "    ret i32 0\n"
     "}";
+
+  // TODO: implement this.
 
   return success(MOCK_IR_CONTENT);
 }
@@ -59,16 +69,14 @@ static auto determine_intermediate_directory(Settings const& settings) -> Path
   return DEFAULT_INTERMEDIATE_DIRECTORY;
 }
 
-static auto ensure_intermediate_directory_exists(Settings const& settings) -> void
+static auto ensure_exists(Path const& directory_path) -> void
 {
   namespace fs = std::filesystem;
-
-  auto path = determine_intermediate_directory(settings);
-  if (fs::is_directory(path)) {
+  if (fs::is_directory(directory_path)) {
     return;
   }
 
-  fs::create_directories(path);
+  fs::create_directories(directory_path);
 }
 
 static auto cleanup_intermediate_directory(Settings const& settings) -> void
@@ -80,6 +88,28 @@ static auto cleanup_intermediate_directory(Settings const& settings) -> void
     return;
 
   fs::remove_all(path);
+}
+
+static auto run_llvm_compilation(Path const& directory) -> void
+{
+  namespace fs = std::filesystem;
+  // TODO: make this configurable.
+
+  auto cwd = fs::current_path();
+  fs::current_path(directory);
+  std::system("clang++ main.ll -o main.exe");
+  fs::current_path(cwd);
+}
+
+static auto generate_intermediate_content(Path const& dir_path, StringView content) -> void
+{
+  // Save the IR file.
+  // TODO: make this configurable.
+  auto const ir_file = dir_path / "main.ll";
+  core::overwrite_file(ir_file, content);
+
+  // Run the compilation
+  run_llvm_compilation(dir_path);
 }
 
 } // namespace rigc::compiler
