dd1f6770322b6228e70a091f17e97ad325ac8e1e
PoetaKodu <poetakodupl@gmail.com>
2023-06-19 21:20:52 +0200

    refactor: better test suite names fix: missing `Empty.jet` test file

--- Projects/Test/Private/Parser.Test.cpp
+++ Projects/Test/Private/Parser.Test.cpp
@@ -1,26 +1,60 @@
 #include "./Common.hpp"
 
-TEST(Parser, empty_module_returns_error)
+// root folder
+
+TEST(Parse_General, empty_module_fails)
 {
   test_module_parse("Empty.jet", false);
 }
 
-TEST(Parser, empty_main_succeeds)
+TEST(Parse_General, empty_main_succeeds)
 {
   test_module_parse("EmptyMain.jet");
 }
 
-TEST(Parser, hello_world_succeeds)
+TEST(Parse_General, hello_world_succeeds)
 {
   test_module_parse("HelloWorld.jet");
 }
 
-TEST(Parser, empty_function_call_succeeds)
+// "functions" subfolder
+
+TEST(Parse_Functions, empty_function_call_succeeds)
 {
   test_module_parse("functions/EmptyFunctionCall.jet");
 }
 
-TEST(Parser, function_in_function_succeeds)
+TEST(Parse_Functions, function_in_function_succeeds)
 {
   test_module_parse("functions/FunctionInFunction.jet");
 }
+
+// "variables" subfolder
+
+TEST(Parse_Variables, single_variable_i32_succeeds)
+{
+  test_module_parse("variables/SingleVariable-i32.jet");
+}
+
+TEST(Parse_Variables, single_variable_f32_succeeds)
+{
+  test_module_parse("variables/SingleVariable-f32.jet");
+}
+
+// "types" subfolder
+
+TEST(Parse_Types, empty_struct_succeeds)
+{
+  test_module_parse("types/EmptyStruct.jet");
+}
+
+TEST(Parse_Types, simple_Vector2_struct_succeeds)
+{
+  test_module_parse("types/Vector2-Simple.jet");
+}
+
+TEST(Parse_Types, Vector2_struct_succeeds)
+{
+  test_module_parse("types/Vector2.jet");
+}
+
